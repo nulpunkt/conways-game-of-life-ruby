0bd6eaaa228fda45198ede627ff2863f04292f6f
Jesper Skovgaard Nielsen <jesper@runlevel0.dk>
2014-04-13 16:55:50 +0200

    added glider

--- conway.rb
+++ conway.rb
@@ -3,7 +3,7 @@ require './lib/curses_screen'
 require './lib/start_positions'
 require './lib/game'
 
-p = StartPositions.new.blinker
+p = StartPositions.new.glider
 g = Game.new
 s = CursesScreen.new g
 
--- lib/start_positions.rb
+++ lib/start_positions.rb
@@ -6,4 +6,19 @@ class StartPositions
     start.alive! 2, 3
     start
   end
+
+# .....
+# ..*..
+# ...*.
+# .***.
+  def glider
+    start = Grid.new 70, 30
+    start.alive! 2, 1
+    start.alive! 3, 2
+    start.alive! 1, 3
+    start.alive! 2, 3
+    start.alive! 3, 3
+    start
+  end
 end
+
