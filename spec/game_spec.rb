081a81f0e312d0e69e1f3377c17dd2bd4b05eba5
Jesper Skovgaard Nielsen <jesper@runlevel0.dk>
2014-04-13 16:14:18 +0200

    A static test more

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,7 +1,19 @@
 require	'./lib/game'
 
 describe Game do
-  it "increments the game" do
+  it "does the Block" do
+    start = Grid.new
+    start.alive! 1, 1
+    start.alive! 1, 2
+    start.alive! 2, 1
+    start.alive! 2, 2
+
+    game = Game.new 5, 5
+
+    game.step(start).should be_eq start
+  end
+
+  it "does the Blinker" do
     start = Grid.new
     start.alive! 2, 1
     start.alive! 2, 2
